494fe32df1766c468050b8e4ad6ab87d6ccea49b
Vishal Gorai <vishalgorai09@gmail.com>
2024-09-19 11:23:17 +0530

    added switch statements

--- Control-Structures/Loops.go
+++ Control-Structures/Loops.go
@@ -13,11 +13,22 @@ func main() {
 
 	// fmt.Println(" ---- ")
 	
-	for i:=1; i<=10; i++ {
-		if (i%2 == 0){
-			fmt.Println(i, " -> Even")
-		} else {
-			fmt.Println(i, " -> Odd")
+	// for i:=1; i<=10; i++ {
+	// 	if (i%2 == 0){
+	// 		fmt.Println(i, " -> Even")
+	// 	} else {
+	// 		fmt.Println(i, " -> Odd")
+	// 	}
+	// }
+
+	for i:=1; i<=7; i++ {
+		switch i {
+		case 1: fmt.Println("One")
+		case 2: fmt.Println("Two")
+		case 3: fmt.Println("Three")
+		case 4: fmt.Println("Four")
+		case 5: fmt.Println("Five")
+		default : fmt.Println("Unkown Number")
 		}
 	}
 
